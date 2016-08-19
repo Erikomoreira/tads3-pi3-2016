bee76ffd1e97e006def973c9671d7b668fe820ea
erik.omoreira <erik.omoreira@CAS555104W0725.senacsp.edu.br>
2016-08-19 20:28:14 -0300

    Segundo Commit

--- agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
+++ agenda/src/main/java/br/senac/tads3/agenda/Agenda.java
@@ -13,7 +13,7 @@ public class Agenda {
     
     public static void main (String args []){
         
-        System.out.println("Hello Word");
+        System.out.println("Hello");
     }
     
     
